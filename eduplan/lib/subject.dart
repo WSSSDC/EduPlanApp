b88e89f13d57550f9486ee927968c1e31dce0913
GreyAerin <bramogus@gmail.com>
2021-01-30 17:02:31 -0500

    Strings into Students

--- eduplan/lib/subject.dart
+++ eduplan/lib/subject.dart
@@ -1,7 +1,8 @@
-import 'student.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'student.dart';
 
 class Subject {
+  static FirebaseFirestore _instance = FirebaseFirestore.instance;
   String id = "";
   String title = "";
   List<String> studentRefs = [];
@@ -11,8 +12,20 @@ class Subject {
     this.id = data.id;
     this.title = data.data()['title'];
     this.studentRefs = data.data()['students'];
-
+    _getStudents();
     //TODO: Conv list of student refs (Strings) to student objects
   }
-}
 
+  _getStudents() async {
+    this.students =
+        await Future.wait(studentRefs.map((e) async => await _getStudent(e)));
+  }
+
+  Future<Student> _getStudent(String docid) async {
+    return _instance
+        .collection("users")
+        .doc(docid)
+        .get()
+        .then((data) => Student.data(data));
+  }
+}
