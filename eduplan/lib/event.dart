d7b4f590ccf68f353e19e6faf120d0459f9e204d
GreyAerin <bramogus@gmail.com>
2021-01-30 16:35:58 -0500

    Converted timestamp -> Datetime

--- eduplan/lib/event.dart
+++ eduplan/lib/event.dart
@@ -4,15 +4,19 @@ class Event {
   String id = "";
   String title = "";
   String desc = "";
-  DateTime date = DateTime.now();
+  Timestamp date;
+  int compTime = 0;
   bool isTest = false;
+  DateTime trueDate = DateTime.now();
 
   Event.data(DocumentSnapshot data) {
     this.id = data.id;
     this.title = data.data()['title'];
     this.desc = data.data()['desc'];
     this.isTest = data.data()['isTest'];
-
+    this.date = data.data()['date'];
+    this.compTime = data.data()['compTime'];
+    trueDate = DateTime.fromMicrosecondsSinceEpoch(date.microsecondsSinceEpoch);
     //TODO: Convert timestamp to datetime
   }
 }
